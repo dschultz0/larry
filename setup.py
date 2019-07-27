bc6014a4fd04a514d11b071dbe8f842f0b286698
dschultz0 <djschult@gmail.com>
2019-07-27 10:40:29 -0700

    Added package distribution

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="awslarry",
+    version="0.0.1",
+    author="Dave Schultz",
+    author_email="djschult@gmail.com",
+    description="Library of helper modules for common data tasks using AWS resources such as S3, SQS, MTurk and others",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dschultz0/awslarry",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
